1110cd6da6722fe4f8a78590d0b0cf26631bbe0b
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-03-21 16:51:11 +0900

    전송

--- ButtonFun02/ViewController.swift
+++ ButtonFun02/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var labelB: UILabel!
+    @IBOutlet weak var labelC: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+}
+    @IBAction func buttonpress(_ sender: Any) {
+        count = count+1
+    labelC.text = String(count)
+        mylabel.text = "A"
     }
 
-
+    @IBAction func buttonBpressed(_ sender: Any) {
+   labelB.text = "B"
+    }
+    
 }
 
